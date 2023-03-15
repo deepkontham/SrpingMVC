8817a77640ddfd9e43c8314710aa2223e20e33ba
pradeepkontham <pradeepkontham@DEEP123>
2023-03-15 09:20:55 +0530

    spring mvc demo

---
+++ 01SpringMVC/src/main/java/com/client/Control.java
@@ -0,0 +1,17 @@
+package com.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Control {
+	
+    @RequestMapping("/hi")
+ 	public String getMsg() {
+		
+		return "welcome";
+		
+	}
+
+}
